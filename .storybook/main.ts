ee0e1578d809a11edfe798ab1116e29d2ca2b7f1
silentFellow <thegreatestintelligent+github@gmail.com>
2024-10-11 22:55:55 +0530

    storybook inital configuration along with default cleanups

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsConfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")]
+      })
+    );
+
+    return config
+  }
+};
+
+export default config;
