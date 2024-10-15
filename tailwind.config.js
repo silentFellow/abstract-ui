157760412f611183344f95a75782372d75dccabf
silentFellow <thegreatestintelligent+github@gmail.com>
2024-10-15 20:18:57 +0530

    fix: tailwindcss-animate import to es6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["selector"],
@@ -53,6 +55,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-  // prefix: "abstract-ui-"
+  plugins: [tailwindcssAnimate],
 };
